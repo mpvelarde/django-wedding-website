6b73677abc19233163c0dc9d7f78f6a8cab91f10
Maria Paz Velarde <1542391+mpvelarde@users.noreply.github.com>
2019-06-30 19:12:44 +0100

    Remove not needed module storages

--- bigday/settings.py
+++ bigday/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
     'guests',
 ]
 
@@ -136,7 +135,6 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join('bigday', 'static'),
