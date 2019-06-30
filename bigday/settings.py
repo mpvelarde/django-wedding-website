438fe954671bc1fa6ae7bcf29c3dfc6e68d241e6
Maria Velarde <1542391+mpvelarde@users.noreply.github.com>
2019-06-30 16:56:36 +0000

    Add https to django settings

--- bigday/settings.py
+++ bigday/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','167.71.170.214','danielandmaria.com']
+ALLOWED_HOSTS = ['127.0.0.1','167.71.170.214','danielandmaria.com','www.danielandmaria.com']
 
 
 # Application definition
