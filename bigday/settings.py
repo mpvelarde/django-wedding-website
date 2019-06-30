d43c39c7ca6accea760b8c7e3b49fe85d2810999
Maria Velarde <1542391+mpvelarde@users.noreply.github.com>
2019-06-30 13:48:49 +0000

    Move secret settings to uncommited file

--- bigday/settings.py
+++ bigday/settings.py
@@ -17,6 +17,7 @@ except ImportError:
     pass
 
 from django.utils.translation import gettext_lazy as _
+from secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,8 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-----------------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -137,6 +136,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join('bigday', 'static'),
@@ -153,7 +153,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'danielandmaria2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'your account’s password'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
