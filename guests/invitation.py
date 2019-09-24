17e06ba99287a29a0026a8de11faaabf94dc88b1
Maria Paz Velarde <1542391+mpvelarde@users.noreply.github.com>
2019-09-24 20:28:36 +0100

    Fetch invitation using invitation model

--- guests/invitation.py
+++ guests/invitation.py
@@ -6,20 +6,16 @@ from django.core.mail import EmailMultiAlternatives
 from django.urls import reverse
 from django.http import Http404
 from django.template.loader import render_to_string
-from guests.models import Party, MEALS
+from guests.models import Party, MEALS, Invitation
 
 INVITATION_TEMPLATE = 'guests/email_templates/invitation.html'
 
 
 def guess_party_by_invite_id_or_404(invite_id):
     try:
-        return Party.objects.get(invitation_id=invite_id)
-    except Party.DoesNotExist:
-        if settings.DEBUG:
-            # in debug mode allow access by ID
-            return Party.objects.get(id=int(invite_id))
-        else:
-            raise Http404()
+        return Invitation.objects.get(invitation_id=invite_id).party
+    except Invitation.DoesNotExist:
+        raise Http404()
 
 
 def get_invitation_context(party):
