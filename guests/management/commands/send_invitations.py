e3c815e4a342ac93fe65fdaf0c09d5b1ec7a3c1c
DESKTOP-U22SJMM\culze <culzeanswift@gmail.com>
2019-10-19 17:20:10 +0100

    Update commands for generate invitations and send invitations. Currently only sending invitations if you specify the party type field.

--- guests/management/commands/send_invitations.py
+++ guests/management/commands/send_invitations.py
@@ -1,7 +1,7 @@
 from optparse import make_option
 from django.core.management import BaseCommand
 from guests import csv_import
-from guests.invitation import send_all_invitations
+from guests.invitation import send_all_invitations, send_invitations_for_event
 from guests.save_the_date import send_all_save_the_dates, clear_all_save_the_dates
 
 
@@ -29,8 +29,15 @@ class Command(BaseCommand):
             default=False,
             help="Reset sent flags"
         )
+        parser.add_argument(
+            '--event-type',
+            type=str,
+            dest='event-type',
+            default='both',
+            help="Send to only this type of event"
+        )
 
     def handle(self, *args, **options):
         if options['reset']:
             clear_all_save_the_dates()
-        send_all_invitations(test_only=not options['send'], mark_as_sent=options['mark_sent'])
+        send_invitations_for_event(test_only=not options['send'], mark_as_sent=options['mark_sent'], party_type=options['event-type'])
